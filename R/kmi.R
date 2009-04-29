3366c62d864917f66831bd7ac160a6eadb227b67
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-04-29 16:17:00 +0200

    * Change in the user interface. Now using a formula
    * Change in the function structure
    kmi() now calls internal functions that distinguish the times
    that need imputation depending on the presence of time-dependent
    covariates
    
    * Speed improvement
    Moving of 1 declarations (findInterval) outside the loops

--- R/kmi.R
+++ R/kmi.R
@@ -1,82 +1,49 @@
-kmi <- function(data, time.col, event.col, id.col = NULL, nimp = 10,
-                failcode = 1, censcode = 0, epsilon = 1,
+kmi <- function(formula, data, id, etype, nimp = 10, epsilon = 1,
                 bootstrap = FALSE, nboot = 10, index) {
-    if (!is.null(id.col)) {
-        info <- match(c(time.col, event.col, id.col), names(data))
-        wdata <- data[order(data[, id.col], data[, time.col]), ]
-        masque <- rbind(1, apply(as.matrix(wdata[, id.col]), 2, diff))
-        masque <- c(masque[-1], 1)
-        wdata <- wdata[masque != 0, ]
-        cens.times <- sort(unique(wdata[wdata[, event.col] == censcode, time.col]))
-        wtimes <- wdata[, time.col]
-        wevent <- wdata[, event.col]
+    Call <- match.call()
+    if ((mode(Call[[2]]) == 'call' &&  Call[[2]][[1]] == as.name('Surv'))
+        || inherits(formula, 'Surv'))  {
+        stop("'kmi' requires a formula as the first argument")
+    }
+    if (missing(etype)) stop()
+    mfnames <- c('formula', 'data', 'id', 'etype')
+    temp <- Call[c(1, match(mfnames, names(Call), nomatch=0))]
+    temp[[1]] <- as.name("model.frame")
+    m <- eval.parent(temp)
+    n <- nrow(m)
+    Y <- model.extract(m, 'response')
+    if (!is.Surv(Y)) stop("Response must be a survival object")
+    id <- model.extract(m, "id")
+    etype <- model.extract(m, "etype")
+    if (attr(Y, "type") == "counting" && !is.null(id)) {
+        toimpute <- kmi.tdp(Y, id = id, etype = etype, failcode = failcode,
+                                  bootstrap = bootstrap, nboot = nboot)
     }
     else {
-        info <- match(c(time.col, event.col), names(data))
-        wdata <- data
-        cens.times <- sort(unique(wdata[wdata[, event.col] == censcode, time.col]))
-        wtimes <- wdata[, time.col]
-        wevent <- wdata[, event.col]
+        toimpute <- kmi.classic(Y, etype = etype, failcode = failcode,
+                                  bootstrap = bootstrap, nboot = nboot)
     }
-    ## w... times and events we'll work with
-    times <- data[, time.col]
-    event <- data[, event.col]
-    ind <- which(event %in% c(censcode, failcode))
-    itimes <- times[-ind] # times to impute
-    otimes <- times[ind] # other times (i.e., event of interest or censoring)
-    if (bootstrap) {
-        if (missing(index)) {
-            index <- lapply(seq_len(nboot), function(k) {
-                sample(seq_len(nrow(wdata)), nrow(wdata),
-                       replace = TRUE)
-            })
-        }
-        g <- matrix(0, nrow = nboot, ncol = length(cens.times))
-        for (l in seq_len(nboot)) {
-            tmp <- summary(survfit(Surv(wtimes[index[[l]]], wevent[index[[l]]] == censcode) ~1))
-            ordre <- findInterval(cens.times, tmp$time)
-            ordre[ordre == 0] <- NA
-            g[l, ] <- tmp$surv[ordre]
-            g[l, ][is.na(g[l, ])] <- 1
-        }
-        g <- apply(g, 2, mean)
-        gg <- c(1, g)
-    }
-    else {
-        g <- summary(survfit(Surv(wtimes, wevent == censcode) ~ 1))
-        gg <- c(1, g$surv)
-    }  
-    a <- FALSE
-    if (wevent[which.max(wtimes)] != censcode) {
-        wtimes <- c(wtimes, max(wtimes) + epsilon)
-        cens.times <- c(cens.times, max(wtimes) + epsilon)
-        a <- TRUE
-    }
+    itimes <- toimpute$itimes
+    gg <- toimpute$gg
     lg <- length(gg)
+    cens.times <- toimpute$cens.times
+    tmp <- findInterval(itimes, c(0, cens.times))
     res <- lapply(seq_len(nimp), function(i) {
-        tt <- double(length(itimes))
         for (j in seq_along(itimes)) {
-            tmp <- findInterval(itimes[j], c(0, cens.times))
-            tmp[tmp == 0] <- 1
-            spr <- gg / c(gg[1:tmp], rep(gg[tmp], lg - tmp))
+            spr <- gg / c(gg[1:tmp[j]], rep(gg[tmp[j]], lg - tmp[j]))
             wp <- -diff(spr)
             wp <- if (a) c(wp, spr[length(spr)]) else wp
-            print("*************************")
-            print(wp)
-            print("**************************")
             tt[j] <- sample(cens.times, 1, replace = TRUE, prob = wp)
         }
-        newtimes <- c(otimes, tt)
-        newevent <- c(event[ind], rep(censcode, length(tt)))
-        aa <- data.frame(newtimes, newevent)
-        aa
+        newtimes <- c(toimpute$otimes, tt)
+        newevent <- c(etype[toimpute$place], rep(0, length(tt)))
+        matrix(c(newtimes, newevent), ncol = 2)
     })
-    orig.data <- rbind(data[ind, ], data[-ind, ])
-    zzz <- list(data = orig.data,
-                imp.dat = res,
-                info = info,
-                nimp = nimp
-                )
+    orig.data <- rbind(data[toimpute$place, ], data[-toimpute$place, ])
+    zzz <- list(imputed.data = res,
+                original.data = orig.data,
+                call = Call)
     class(zzz) <- "kmi"
     zzz
 }
+        
