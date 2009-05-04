613ee2d3bdada801c814ea96e8f1d01d8882f439
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-05-04 18:14:16 +0200

    Adaptation of cox.kmi to new kmi function
    use of mitools::MIcombine to get the pooled estimates

--- R/cox.kmi.R
+++ R/cox.kmi.R
@@ -1,32 +1,24 @@
-### Wrapper for coxph (survival) to be used
-### with kaplan-meier imputation
-
-cox.kmi <- function(formula, kmi.object, ...) {
-    if (!inherits(kmi.object, "kmi")) {
-        stop("'object' must be of class 'kmi'")
+cox.kmi <- function(formula, x, ...) {
+    if (!inherits(x, "kmi")) {
+        stop("'x' must be of class 'kmi'")
     }
     call <- match.call()
-    nimp <- kmi.object$nimp
-    result <- lapply(seq_len(nimp), function(i) {
-        daten <- kmi.object$data
-        daten[, kmi.object$info[1]] <- kmi.object$imp.dat[[i]]$newtimes
-        daten[, kmi.object$info[2]] <- kmi.object$imp.dat[[i]]$newevent
-        tmp <- coxph(formula, data = daten, ...)
+    info <- x$info
+    result <- lapply(seq_along(x$imputed.data), function(i) {
+        daten <- x$original.data
+        daten[, info[1]] <- x$imputed.data[[i]][, 1]
+        daten[, info[2]] <- x$imputed.data[[i]][, 2]
+        tmp <- coxph(formula, data = daten)
         tmp
     })
-    coef <- var <- list()
-    for (i in seq_len(nimp)) {
-        coef[[i]] <- result[[i]]$coefficients
-        var[[i]] <- result[[i]]$var
-    }
-    coef <- do.call(rbind, coef)
-    est.coef <- apply(coef, 2, mean)
-    va <- apply(do.call(cbind, var), c(1, 2), mean)
-    dim(va) <- c(ncol(coef), ncol(coef), nimp)
-    va <- apply(va, c(1,2), mean)
-    var.coef <- va + (1 + (1/nimp)) * var(coef)
-    zzz <- list(call = call, coef = est.coef, var = var.coef,
-                cox.kmi.fit = result)
+    res <- MIcombine(result)
+    zzz <- list(coefficients = res$coefficients,
+                variance = res$variance,
+                nimp = res$nimp,
+                df = res$df,
+                individual.fit = result)
     class(zzz) <- "cox.kmi"
     zzz
 }
+    
+    
