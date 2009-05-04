1af19d2042ceb33864b6377d587f72b4a61d4318
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-05-04 15:07:46 +0200

    Get names of the times and event columns

--- R/kmi.R
+++ R/kmi.R
@@ -15,11 +15,16 @@ kmi <- function(formula, data, id, etype, failcode = 1, nimp = 10, epsilon = 1,
     if (!is.Surv(Y)) stop("Response must be a survival object")
     id <- model.extract(m, "id")
     etype <- model.extract(m, "etype")
+    aa <- Call[[2]][[2]]
     if (attr(Y, "type") == "counting" && !is.null(id)) {
+        info <- c(as.character(aa[[3]])[as.character(aa[[3]]) %in% names(data)],
+                  as.character(aa[[4]])[as.character(aa[[4]]) %in% names(data)])
         toimpute <- kmi.tdc(Y, id = id, etype = etype, failcode = failcode, 
                             epsilon = epsilon, bootstrap = bootstrap, nboot = nboot)
     }
     else {
+        info <- c(as.character(aa[[2]])[as.character(aa[[2]]) %in% names(data)],
+                  as.character(aa[[3]])[as.character(aa[[3]]) %in% names(data)])
         toimpute <- kmi.classic(Y, etype = etype, failcode = failcode, epsilon = epsilon,
                                 bootstrap = bootstrap, nboot = nboot)
     }
@@ -43,6 +48,7 @@ kmi <- function(formula, data, id, etype, failcode = 1, nimp = 10, epsilon = 1,
     orig.data <- rbind(data[toimpute$place, ], data[-toimpute$place, ])
     zzz <- list(imputed.data = res,
                 original.data = orig.data,
+                info = info,
                 call = Call)
     class(zzz) <- "kmi"
     zzz
