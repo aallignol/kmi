715ee6562b2a95f319e7b8d720c9beef43de5078
Arthur Allignol <arthur.allignol@fdm.uni-freiburg.de>
2009-05-06 15:23:14 +0200

    Finish the test file

--- tests/test.kmi.R
+++ tests/test.kmi.R
@@ -44,6 +44,52 @@ aa <- kmi(Surv(time, ev != 0) ~ 1, data, etype = ev, nimp = 13)
 length(aa$imputed.data) == 13
 
 
-### 
+### test 2
+
+set.seed(198740)
+time <- rexp(100)
+ev <- sample(c(0, 1, 2), 100, replace = TRUE)
+cov <- rbinom(100, 1, 0.5)
+dd <- data.frame(time, ev, cov)
+
+set.seed(1440293)
+dat.kmi <- kmi(Surv(time, ev != 0) ~ 1, dd, etype = ev, nimp = 100)
+
+for (i in 1:3) print(dat.kmi$imputed.data[[i]])
+
+fit.kmi <- cox.kmi(Surv(time, ev == 1) ~ cov, dat.kmi)
+
+fit.kmi
+
+summary(fit.kmi)
 
 
+### test 4
+
+data(icu.pneu)
+
+set.seed(1313)
+dat <- kmi(Surv(entry, exit, status) ~ 1, data = icu.pneu,
+           etype = event, id= id, failcode = 3, nimp = 5)
+
+icu.pneu$ev <- icu.pneu$event
+icu.pneu$ev[icu.pneu$status == 0] <- 0
+
+set.seed(1313)
+dat2 <- kmi(Surv(entry, exit, ev != 0) ~ 1, data = icu.pneu,
+           etype = ev, id= id, failcode = 3, nimp = 5)
+
+a <- logical(5)
+for (i in 1:5) a[i] <- all.equal(dat$imputed.data[[i]][, 1], dat2$imputed.data[[i]][, 1])
+a
+
+fit.kmi <- cox.kmi(Surv(entry, exit, event == 3) ~ pneu, dat)
+
+fit.kmi2 <- cox.kmi(Surv(entry, exit, ev == 3) ~ pneu, dat2)
+
+all.equal(fit.kmi$coefficients, fit.kmi2$coefficients)
+all.equal(fit.kmi$variance, fit.kmi2$variance)
+
+fit.kmi
+
+fit.kmi2
