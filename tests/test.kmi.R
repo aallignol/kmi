8d5580f7215b32f1f6d72889afc18431c100e1e7
Arthur Allignol <arthur@kaspara.kaspara>
2010-05-26 14:26:39 +0200

    Add tests to be sure that specifying etype as a factor works

--- tests/test.kmi.R
+++ tests/test.kmi.R
@@ -52,27 +52,59 @@ ev <- sample(c(0, 1, 2), 100, replace = TRUE)
 cov <- rbinom(100, 1, 0.5)
 dd <- data.frame(time, ev, cov)
 
+## add a test when etype is a factor
+dd$status <- factor(ifelse(dd$ev == 0, "cens", ifelse(dd$ev == 1, "rel", "dc")))
+
 set.seed(1440293)
 dat.kmi <- kmi(Surv(time, ev != 0) ~ 1, dd, etype = ev, nimp = 5)
+set.seed(1440293)
+dat.kmi.fact <- kmi(Surv(time, status != "cens") ~ 1, dd, etype = status,
+                    nimp = 5, failcode = "rel")
+set.seed(1440293)
+dat.kmi.mixed1 <- kmi(Surv(time, ev != 0) ~ 1, dd, etype = status,
+                      nimp = 5, failcode = "rel")
+set.seed(1440293)
+dat.kmi.mixed2 <- kmi(Surv(time, status != "cens") ~ 1, dd, etype = ev,
+                      nimp = 5, failcode = 1)
+
 
 fit.kmi <- cox.kmi(Surv(time, ev == 1) ~ cov, dat.kmi)
+fit.kmi.mixed1 <- cox.kmi(Surv(time, status == "rel") ~ cov, dat.kmi.mixed1)
+fit.kmi.mixed2 <- cox.kmi(Surv(time, ev == 1) ~ cov, dat.kmi.mixed2)
+
+all.equal(coef(fit.kmi), coef(fit.kmi.fact))
+all.equal(coef(fit.kmi), coef(fit.kmi.mixed1))
+all.equal(coef(fit.kmi), coef(fit.kmi.mixed2))
+all.equal(fit.kmi$variance, fit.kmi.fact$variance)
+all.equal(fit.kmi$variance, fit.kmi.mixed1$variance)
+all.equal(fit.kmi$variance, fit.kmi.mixed2$variance)
 
 fit.kmi
 
 summary(fit.kmi)
+
+fit.kmi.fact
+
+summary(fit.kmi.fact)
 
 ## avec bootstrap
 
 set.seed(867988)
 dat.kmib <- kmi(Surv(time, ev != 0) ~ 1, dd, etype = ev, nimp = 5,
                 boot = TRUE, nboot = 5)
+set.seed(867988)
+dat.kmib.fact <- kmi(Surv(time, ev != 0) ~ 1, dd, etype = status, nimp = 5,
+                     boot = TRUE, nboot = 5, failcode = "rel")
 
 fit.kmib <- cox.kmi(Surv(time, ev == 1) ~ cov, dat.kmib)
+fit.kmib.fact <- cox.kmi(Surv(time, status == "rel") ~ cov, dat.kmib.fact)
+
+all.equal(coef(fit.kmib), coef(fit.kmib.fact))
 
 fit.kmib
+fit.kmib.fact
 
 summary(fit.kmib)
-
 
 
 ### test 4
@@ -83,12 +115,19 @@ set.seed(1313)
 dat <- kmi(Surv(entry, exit, status) ~ 1, data = icu.pneu,
            etype = event, id= id, failcode = 3, nimp = 5)
 
+### add a factor for testing purposes
+icu.pneu$ev.fact <- factor(ifelse(icu.pneu$event == 2, "disch", "death"))
+
 icu.pneu$ev <- icu.pneu$event
 icu.pneu$ev[icu.pneu$status == 0] <- 0
 
 set.seed(1313)
 dat2 <- kmi(Surv(entry, exit, ev != 0) ~ 1, data = icu.pneu,
            etype = ev, id= id, failcode = 3, nimp = 5)
+
+set.seed(1313)
+dat3 <- kmi(Surv(entry, exit, status) ~ 1, data = icu.pneu,
+            etype = ev.fact, id = id, failcode = "death", nimp = 5)
 
 a <- logical(5)
 for (i in 1:5) a[i] <- all.equal(dat$imputed.data[[i]][, 1], dat2$imputed.data[[i]][, 1])
@@ -98,12 +137,17 @@ fit.kmi <- cox.kmi(Surv(entry, exit, event == 3) ~ pneu, dat)
 
 fit.kmi2 <- cox.kmi(Surv(entry, exit, ev == 3) ~ pneu, dat2)
 
+fit.kmi3 <- cox.kmi(Surv(entry, exit, ev.fact == "death") ~ pneu, dat3)
+
 all.equal(fit.kmi$coefficients, fit.kmi2$coefficients)
+all.equal(coef(fit.kmi), coef(fit.kmi3))
 all.equal(fit.kmi$variance, fit.kmi2$variance)
 
 fit.kmi
 
 fit.kmi2
+
+fit.kmi3
 
 ## avec bootstrap
 
@@ -117,6 +161,12 @@ dat2 <- kmi(Surv(entry, exit, ev != 0) ~ 1, data = icu.pneu,
             etype = ev, id= id, failcode = 3, nimp = 5,
             boot = TRUE, nboot = 5)
 
+set.seed(598085)
+dat3 <- kmi(Surv(entry, exit, status) ~ 1, data = icu.pneu,
+            etype = ev.fact, id = id, failcode = "death", nimp = 5,
+            boot = TRUE, nboot = 5)
+
+
 a <- logical(5)
 for (i in 1:5) a[i] <- all.equal(dat$imputed.data[[i]][, 1], dat2$imputed.data[[i]][, 1])
 a
@@ -125,9 +175,14 @@ fit.kmi <- cox.kmi(Surv(entry, exit, event == 3) ~ pneu, dat)
 
 fit.kmi2 <- cox.kmi(Surv(entry, exit, ev == 3) ~ pneu, dat2)
 
+fit.kmi3 <- cox.kmi(Surv(entry, exit, ev.fact == "death") ~ pneu, dat3)
+
 all.equal(fit.kmi$coefficients, fit.kmi2$coefficients)
+all.equal(coef(fit.kmi), coef(fit.kmi3))
 all.equal(fit.kmi$variance, fit.kmi2$variance)
 
 fit.kmi
 
 fit.kmi2
+
+fit.kmi3
